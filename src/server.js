88e0250e44651bebbe30b4e4c1918dd3013ee316
Danny Denenberg <dannydenenberg@gmail.com>
2020-08-17 10:12:10 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -56,10 +56,11 @@ const app = express();
 /** Redirect from http to https **/
 if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
       next()
+    }
   })
 }
 
