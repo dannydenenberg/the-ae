d24cdebbb739cdfadec1dd45ce972fc4cc49e93d
Danny Denenberg <dannydenenberg@gmail.com>
2020-10-27 22:11:17 -0500

    editing verify

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,6 +8,7 @@ import Category from "./../models/category.model";
 import express from "express";
 import { VERIFY_TOKEN_EXPRESS, generateToken } from "./jwt";
 import { JWT_COOKIE_NAME } from "./constants";
+import { sendVerificationCodeEmail } from "./email";
 
 let router = express.Router();
 
@@ -20,7 +21,7 @@ router.get("/categories", (req, res) => {
   Category.find({}, (err, docs) => {
     if (err)
       throw new Error(
-        "Couldn't find any categorical data. -- NOTE SUPPOSED TO HAPPEN"
+        "Couldn't find any categorical data. -- NOTE SUPPOSED TO HAPPEN",
       );
     res.json(docs);
   });
@@ -79,6 +80,70 @@ router.post("/logon", (req, res) => {
           res.json({ error: true, message: "passwords dont match." });
         }
       });
+    }
+  });
+});
+
+// _id and code are gotten from the URL queries of /verify
+// this updates the user's `verified` field to true
+router.post("/verifyuser", (req, res) => {
+  let { _id, code } = req.params;
+  User.findById(_id, (err, doc) => {
+    if (err) {
+      res.json({
+        error: true,
+        message: "111 Error in finding document--NOT SUPPOSED TO HAPPEN.",
+      });
+      return;
+    }
+
+    // is the supplied code in the list of usable codes
+    if (doc.verificationCodes.includes(code)) {
+      // set verified to true
+      User.findOneAndUpdate({ _id }, { verified: true }, (err, doc, res) => {
+        if (err) {
+          res.json({
+            error: true,
+            message: "222 Error in finding document -- NOT SUPPOSED TO HAPPEN.",
+          });
+          console.log(err);
+          return;
+        }
+        res.json({ error: false, message: "Verified." });
+      });
+    } else {
+      res.json({ error: true, message: "Code not found." });
+    }
+  });
+});
+
+/**
+BODY:
+
+{
+  user: {
+    email,
+    password
+  }
+}
+ */
+router.post("/makeperson", (req, res) => {
+  let { user } = req.body;
+  let newPerson = new User(user);
+
+  // send a verification email to the person
+  let code = newPerson.verificationCodes[0];
+  sendVerificationCodeEmail(newPerson._id, code);
+
+  newPerson.save((err) => {
+    if (err) {
+      res.json({
+        error: true,
+        message: "333 Error in saving user in database.",
+      });
+      console.log(err);
+    } else {
+      res.json({ error: false, ...newPerson });
     }
   });
 });
